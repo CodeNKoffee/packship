35896c4918fcd95c155dac7e59f1dcfc86e4f49a
Hatem Soliman <hatemthedev@gmail.com>
2024-08-28 03:21:57 +0300

    Removed types folder

--- types/index.ts
+++
@@ -1,6 +0,0 @@
-// This file is intended for exporting types and interfaces for the zozo package.
-
-// Export your types and interfaces here
-export interface ExampleInterface {
-  // Add your properties here
-}
