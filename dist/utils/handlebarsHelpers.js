4b36b054d599eca7d55445b0de91d8ddf861d0e1
Hatem Soliman <hatemthedev@gmail.com>
2024-09-02 14:04:13 +0300

    Divided files into subparts

--- dist/utils/handlebarsHelpers.js
+++
@@ -1,9 +0,0 @@
-import Handlebars from 'handlebars';
-export function registerHandlebarsHelpers() {
-    Handlebars.registerHelper('if_eq', function (a, b, options) {
-        return a === b ? options.fn(this) : options.inverse(this);
-    });
-    Handlebars.registerHelper('toLowerCase', function (str) {
-        return str.toLowerCase();
-    });
-}
