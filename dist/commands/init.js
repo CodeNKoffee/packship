ba4bcc3dde36ce79e42063a1cde87a51f36230cc
Hatem Soliman <hatemthedev@gmail.com>
2024-09-02 17:51:22 +0300

    More

---
+++ dist/commands/init.js
@@ -0,0 +1,10 @@
+import { Command } from 'commander';
+import { createPackage } from '../utils/createPackage.js';
+const initCommand = new Command("init");
+initCommand
+    .description('Initialize a new npm package')
+    .action(async () => {
+    const packageName = await createPackage();
+    console.log(`Initialized your new npm package: ${packageName}\n\nHappy packshipping! 📦🛻💨\n`);
+});
+export default initCommand;
--- src/utils/fileGeneration.ts
+++ src/utils/fileGeneration.ts
@@ -6,19 +6,27 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+Handlebars.registerHelper('JSONstringify', function(context) {
+  return JSON.stringify(context);
+});
+
 export async function generateFiles(files: any[], packageData: any, packageDir: string) {
   for (const file of files) {
-    try {
-      const templatePath = path.join(__dirname, '..', 'templates', file.template);
-      const templateContent = await fs.promises.readFile(templatePath, 'utf-8');
-      const compiledTemplate = Handlebars.compile(templateContent);
-      const fileContent = compiledTemplate(packageData);
+    const templatePath = path.join(__dirname, '..', 'templates', file.template);
+    const filePath = path.join(packageDir, file.name);
+    const dirName = path.dirname(filePath);
 
-      const filePath = path.join(packageDir, file.name);
-      await fs.promises.mkdir(path.dirname(filePath), { recursive: true });
-      await fs.promises.writeFile(filePath, fileContent, 'utf-8');
-    } catch (error) {
-      console.error(`Error generating file ${file.name}: `, error)
+    // Ensure the directory exists
+    await fs.promises.mkdir(dirName, { recursive: true });
+
+    // Check if the template path exists
+    if (!fs.existsSync(templatePath)) {
+      throw new Error(`Template not found: ${templatePath}`);
     }
+
+    const templateContent = await fs.promises.readFile(templatePath, 'utf8');
+    const fileContent = Handlebars.compile(templateContent)(packageData);
+
+    await fs.promises.writeFile(filePath, fileContent, 'utf8');
   }
 }
