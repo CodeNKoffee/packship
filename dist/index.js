7b84a55dd2378db60e0baee96a90c50b44969988
Hatem Soliman <hatemthedev@gmail.com>
2024-09-15 19:47:26 +0300

    Added publishCommand to index

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 import { Command } from "commander";
 import initCommand from "./commands/init.js";
 import versionCommand, { getVersion } from "./commands/version.js";
+import publishCommand from "./commands/publish.js";
 const program = new Command();
 // Set up the CLI structure
 program
@@ -11,5 +12,6 @@ program
 // Register commands correctly with names
 program.addCommand(initCommand);
 program.addCommand(versionCommand);
+program.addCommand(publishCommand);
 // Parse the command-line arguments
 program.parse(process.argv);
